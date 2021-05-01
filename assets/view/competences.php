3d7a79e2b3810f2001242a38088cc655819cea73
I GenZo I <genzo10@hotmail.fr>
2021-05-01 19:37:28 +0200

    feat: page contact / modif menu et shadow

--- assets/view/competences.php
+++ assets/view/competences.php
@@ -2,5 +2,50 @@
 
 <?php require "header.php"; ?>
 
+<div class="contain_comp">
+    <h1 class="title">Mes compétences</h1>
+    <div class="competences">
+        <div>
+            <div class="competences_graph_title">
+                <h2>HTML / CSS</h2>
+            </div>
+            <div class="bar_competences">
+                <div class="html"></div>
+            </div>
+        </div>
+        <div>
+            <div class="competences_graph_title">
+                <h2>WORDPRESS</h2>
+            </div>
+            <div class="bar_competences">
+                <div class="wordpress"></div>
+            </div>
+        </div>
+        <div>
+            <div class="competences_graph_title">
+                <h2>JAVASCRIPT</h2>
+            </div>
+            <div class="bar_competences">
+                <div class="javascript"></div>
+            </div>
+        </div>
+        <div>
+            <div class="competences_graph_title">
+                <h2>PHP</h2>
+            </div>
+            <div class="bar_competences">
+                <div class="php"></div>
+            </div>
+        </div>
+        <div>
+            <div class="competences_graph_title">
+                <h2>SYMFONY</h2>
+            </div>
+            <div class="bar_competences">
+                <div class="symfony"></div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php require "footer.php"; ?>
--- assets/view/contact.php
+++ assets/view/contact.php
@@ -2,5 +2,25 @@
 
 <?php require "header.php"; ?>
 
+<div class="contain_contact">
+    <h1>Contactez moi</h1>
+    <form id="form_contact" action="" method="post">
+        <div class="contact_name">
+            <input class="form_input" type="text" id="name" name="name" placeholder="Nom" value="">
+            <p class="form_error"></p>
+        </div>
+        <div class="contact_email">
+            <input class="form_input" type="email" id="email" name="email" placeholder="Email" value="">
+            <p class="form_error"></p>
+        </div>
+        <div class="contact_message">
+            <textarea class="form_textarea" id="message" name="message" placeholder="Message"></textarea>
+            <p class="form_error"></p>
+        </div>
+        <div class="contact_submit">
+            <input class="form_submit" type="submit" name="contact_submit" value="Envoyer">
+        </div>
+    </form>
+</div>
 
 <?php require "footer.php"; ?>
